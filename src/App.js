edb994dacec4688832c59a6553cfa1de52311884
Steph Paterson <steph.c.paterson@gmail.com>
2022-02-22 09:47:39 +0000

    updated the classname logic

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import {useState} from 'react';
 function App() {
 
   const [items, setItems] = useState([
-    {name: "Do laundry", priority: "High"},
-    {name: "Plan meals", priority: "High" },
-    {name: "Clean car", priority: "Low"}
+    {name: "Do laundry", priority: "high"},
+    {name: "Plan meals", priority: "high" },
+    {name: "Clean car", priority: "low"}
   ])
 
   const initialValues = {
@@ -17,7 +17,7 @@ function App() {
   const [newItem, setNewItem] = useState(initialValues)
 
   const itemNodes = items.map((item, index) => {
-    return( <li key={index} className ={item.priority === "High" ? "high" : "low"}>
+    return( <li key={index} className ={item.priority}>
       <span>{item.name}</span>
       <span>{item.priority}</span>
     </li> )
@@ -47,8 +47,8 @@ function App() {
       <form onSubmit={saveNewItem}>
         <input type="text" id="new-item" placeholder="New to do item" value={newItem.name} name="name" onChange={handleItemInput}/>
         <div onChange={handleItemInput}>
-          High <input type="radio" value="High" name="priority" /> 
-          Low <input type="radio" value="Low" name="priority" /> 
+          High <input type="radio" value="high" name="priority" /> 
+          Low <input type="radio" value="low" name="priority" /> 
         </div>
         <button type='submit'>Save Item</button>
       </form>
